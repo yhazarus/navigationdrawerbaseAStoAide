50fde52b92e349d37e6e162abb94d565f4da9017
Yhazarus <jumptotheinfinite@gmail.com>
2019-01-22 03:21:07 +0100

    update project

--- instappfeature/src/test/java/com/redilexapps/navigationdrawerbaseastoaide/instappfetature/ExampleUnitTest.java
+++ instappfeature/src/test/java/com/redilexapps/navigationdrawerbaseastoaide/instappfetature/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.redilexapps.navigationdrawerbaseastoaide.instappfetature;
+package com.redilexapps.navigationdrawerbaseastoaide.instappfeature;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
